7c536c90c3509ca98cf8580564804ac3d1aae1ec
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-03-18 21:31:10 +0900

    change label color

--- kibosuta/FirstViewController.swift
+++ kibosuta/FirstViewController.swift
@@ -34,150 +34,136 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var altkouso: UILabel!
     @IBOutlet weak var gtpkouso: UILabel!
     @IBOutlet weak var glucose: UILabel!
-    var hbalcResult: String!
-    var tgRsult = Int.self
-    var hdlResult: String!
-    var tchoResult: String!
-    var astResult: String!
-    var altResult: String!
-    var ggtpResult: String!
-    var gluResult: String!
-
-
-
+//    var hbalcResult: String!
+//    var tgRsult = Int.self
+//    var hdlResult: String!
+//    var tchoResult: String!
+//    var astResult: String!
+//    var altResult: String!
+//    var ggtpResult: String!
+//    var gluResult: String!
 
     @IBAction func kennsa(_ sender: UIButton) {
+        hbalc.text="64"
+        tg.text="140"
+        hdl.text="50"
+        tcho.text="180"
+        ast.text="26"
+        alt.text="27"
+        ggtp.text="80"
+        glu.text="180"
+        hanntei.text="C"
+        hanntei2.text="A"
+        hanntei3.text="B"
+        hanntei4.text="A"
+        hanntei5.text="B"
+        hanntei6.text="C"
+        hanntei7.text="C"
+        hanntei8.text="D"
+        guard let hbalcResult = Int(hbalc.text!), let tgResult = Int(tg.text!), let hdlResult = Int(hdl.text!), let tchoResult = Int(tcho.text!),
+              let astResult = Int(ast.text!), let altResult = Int(alt.text!), let ggtpResult = Int(ggtp.text!), let gluResult = Int(glu.text!) else { return }
 
-//        hanntei.text="C"
-//        hanntei2.text="A"
-//        hanntei3.text="B"
-//        hanntei4.text="A"
-//        hanntei5.text="B"
-//        hanntei6.text="C"
-//        hanntei7.text="C"
-//        hanntei8.text="D"
-        
-        
+        let res = ScanResult(hbalc: hbalcResult, tg: tgResult, hdl: hdlResult, tcho: tchoResult, ast: astResult, alt: altResult, ggtp: ggtpResult, glu: gluResult)
 
-
-        
+        changeUIColor(from: res)
     }
     @IBAction func comehome (segue: UIStoryboardSegue){
         
     }
     
-  
+    func changeUIColor(from result: ScanResult) {
+        switch result.hbalc {
+        case ...10:
+            hbalc.backgroundColor = UIColor.green
+        case ...15:
+            hbalc.backgroundColor = UIColor.white
+        case ...20:
+            hbalc.backgroundColor = UIColor.yellow
+        default:
+            hbalc.backgroundColor = UIColor.red
+        }
+
+        switch result.tg {
+        case ...10:
+            tg.backgroundColor = UIColor.green
+        case ...15:
+            tg.backgroundColor = UIColor.white
+        case ...20:
+            tg.backgroundColor = UIColor.yellow
+        default:
+            tg.backgroundColor = UIColor.red
+        }
+
+        switch result.hdl {
+        case ...10:
+            hdl.backgroundColor = UIColor.green
+        case ...15:
+            hdl.backgroundColor = UIColor.white
+        case ...20:
+            hdl.backgroundColor = UIColor.yellow
+        default:
+            hdl.backgroundColor = UIColor.red
+        }
+
+        switch result.tcho {
+        case ...10:
+            tcho.backgroundColor = UIColor.green
+        case ...15:
+            tcho.backgroundColor = UIColor.white
+        case ...20:
+            tcho.backgroundColor = UIColor.yellow
+        default:
+            tcho.backgroundColor = UIColor.red
+        }
+
+        switch result.ast {
+        case ...10:
+            ast.backgroundColor = UIColor.green
+        case ...15:
+            ast.backgroundColor = UIColor.white
+        case ...20:
+            ast.backgroundColor = UIColor.yellow
+        default:
+            ast.backgroundColor = UIColor.red
+        }
+
+        switch result.alt {
+        case ...10:
+            alt.backgroundColor = UIColor.green
+        case ...15:
+            alt.backgroundColor = UIColor.white
+        case ...20:
+            alt.backgroundColor = UIColor.yellow
+        default:
+            alt.backgroundColor = UIColor.red
+        }
+
+        switch result.ggtp {
+        case ...10:
+            ggtp.backgroundColor = UIColor.green
+        case ...15:
+            ggtp.backgroundColor = UIColor.white
+        case ...20:
+            ggtp.backgroundColor = UIColor.yellow
+        default:
+            ggtp.backgroundColor = UIColor.red
+        }
+
+        switch result.glu {
+        case ...10:
+            glu.backgroundColor = UIColor.green
+        case ...15:
+            glu.backgroundColor = UIColor.white
+        case ...20:
+            glu.backgroundColor = UIColor.yellow
+        default:
+            glu.backgroundColor = UIColor.red
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let hbalcResult:Int? = Int(hbalc.text!)
-        let tgRsult:Int? = Int(tg.text!)
-        let hdlResult:Int? = Int(hdl.text!)
-        let tchoResult:Int? = Int(tcho.text!)
-        let astResult:Int? = Int(ast.text!)
-        let altResult:Int? = Int(alt.text!)
-        let ggtpResult:Int? = Int(ggtp.text!)
-        let gluResult:Int? = Int(glu.text!)
-        
-        if let result1 = hbalcResult {
-            if result1 <= 10 {
-                hbalc.backgroundColor = UIColor.green
-            } else if result1 <= 15 {
-                hbalc.backgroundColor = UIColor.white
-            } else if result1 <= 20{
-                hbalc.backgroundColor = UIColor.yellow
-            } else if result1 <= 25{
-                hbalc.backgroundColor = UIColor.red
-            }
-        }
-        if let result2 = tgRsult {
-            if result2 <= 10 {
-            tg.backgroundColor = UIColor.green
-            } else if result2 <= 15 {
-                tg.backgroundColor = UIColor.white
-            } else if result2 <= 20{
-                tg.backgroundColor = UIColor.yellow
-            } else if result2 <= 25{
-                tg.backgroundColor = UIColor.red
-            }
-        }
-        
-        if let result3 = hdlResult {
-            if result3 <= 10 {
-                hdl.backgroundColor = UIColor.green
-            } else if result3 <= 15 {
-                hdl.backgroundColor = UIColor.white
-            } else if result3 <= 20{
-                hdl.backgroundColor = UIColor.yellow
-            } else if result3 <= 25{
-                hdl.backgroundColor = UIColor.red
-            }
-        }
-        if let result4 = tchoResult {
-            if result4 <= 10 {
-                tcho.backgroundColor = UIColor.green
-            } else if result4 <= 15 {
-                tcho.backgroundColor = UIColor.white
-            } else if result4 <= 20{
-                tcho.backgroundColor = UIColor.yellow
-            } else if result4 <= 25{
-                tcho.backgroundColor = UIColor.red
-            }
-        }
-        if let result5 = astResult {
-            if result5 <= 10 {
-                ast.backgroundColor = UIColor.green
-            } else if result5 <= 15 {
-                ast.backgroundColor = UIColor.white
-            } else if result5 <= 20{
-                ast.backgroundColor = UIColor.yellow
-            } else if result5 <= 25{
-                ast.backgroundColor = UIColor.red
-            }
-        }
-        if let result6 = altResult {
-            if result6 <= 10 {
-                alt.backgroundColor = UIColor.green
-            } else if result6 <= 15 {
-                alt.backgroundColor = UIColor.white
-            } else if result6 <= 20{
-                alt.backgroundColor = UIColor.yellow
-            } else if result6 <= 25{
-                alt.backgroundColor = UIColor.red
-            }
-        }
-        if let result7 = ggtpResult {
-            if result7 <= 10 {
-                ggtp.backgroundColor = UIColor.green
-            } else if result7 <= 15 {
-                ggtp.backgroundColor = UIColor.white
-            } else if result7 <= 20{
-                ggtp.backgroundColor = UIColor.yellow
-            } else if result7 <= 25{
-                ggtp.backgroundColor = UIColor.red
-            }
-        }
-        if let result8 = gluResult {
-            if result8 <= 10 {
-                glu.backgroundColor = UIColor.green
-            } else if result8 <= 15 {
-                glu.backgroundColor = UIColor.white
-            } else if result8 <= 20{
-                glu.backgroundColor = UIColor.yellow
-            } else if result8 <= 25{
-                glu.backgroundColor = UIColor.red
-            }
-        }
     }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ kibosuta/ScanResult.swift
@@ -0,0 +1,20 @@
+//
+//  ScanResult.swift
+//  kibosuta
+//
+//  Created by 下村一将 on 2018/03/18.
+//  Copyright © 2018年 Aramata. All rights reserved.
+//
+
+import Foundation
+
+struct ScanResult {
+    let hbalc: Int
+    let tg: Int
+    let hdl: Int
+    let tcho: Int
+    let ast: Int
+    let alt: Int
+    let ggtp: Int
+    let glu: Int
+}
